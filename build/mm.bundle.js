5a55e8a0d937af8d311ffbebe28c725e2a0ace2b
momoLeKebab <mohamad.moutaliev@epitech.eu>
2017-07-12 17:34:07 +0200

    bite

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -14805,14 +14805,14 @@ angular.module("mm.core.courses")
     var cat_id = [];
     var cat_name = [];
     var i = 0;
-/*
+
     while (i != tab.length)
     {
         cat_id.push(tab[i][0]);
         cat_name.push(tab[i][1]);
         i++;
     }
-
+/*
     $scope.catalogue_id = cat_id;
     $scope.catalogue_name = cat_name;*/
 
