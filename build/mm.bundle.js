41a3281f5870ac0422cd653c9117267faef1ca7a
momoLeKebab <mohamad.moutaliev@epitech.eu>
2017-07-17 15:19:17 +0200

    work in progress

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -14720,7 +14720,11 @@ angular.module("mm.core.courses").controller("mmcatalogueCtrl", ["$scope", "$sta
 //    alert($stateParams.id);
 
     // cette requete va permettre de get tout les catalogues
-    sql = "SELECT id, name, CatalogueEntity FROM mdlcourse_categories WHERE idnumber LIKE 'cat-%' AND parent = " + $stateParams.id;
+    if ($stateParams.id == 0)
+        sql = "SELECT id, name, CatalogueEntity FROM mdlcourse_categories WHERE idnumber LIKE 'cat-%'";
+    else
+        sql = "SELECT id, name, CatalogueEntity FROM mdlcourse_categories WHERE parent = " + $stateParams.id;
+
     $http.get(url + "?ip=" + ip + "&login=" + login + "&mdp=" + mdp + "&table=" + table + "&sql=" + sql).success(function(data, status)
     {
         $scope.catalogue = data;
