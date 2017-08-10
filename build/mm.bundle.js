be73a2685aea3f9cf2c4aa7907628c809584f7d3
momoLeKebab <mohamad.moutaliev@epitech.eu>
2017-08-10 14:59:09 +0200

    coursecount

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -14728,6 +14728,7 @@ angular.module("mm.core.courses").controller("mmcatalogueCtrl", ["$scope", "$sta
     {
         // on stock le data dans le scope qui pourra etre appelé dans le .html
         $scope.catalogue = data;
+        $scope.coursecount = "ah";
 
         function coursecount()
         {
